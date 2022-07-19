90b30f43315d86d6fa4ded01ea3e02ab8d4ed4db
Harrison <harrisondcrafter@gmail.com>
2022-07-19 21:54:33 +0100

    Importer will now attach onto the process via PID input and modify the graphic, however texture compression is needed

--- importer.go
+++ importer.go
@@ -17,18 +17,24 @@ func CleanLineString(text string) (string) {
 	return clean
 }
 
-func Select(reader *bufio.Reader) (int) {
-	fmt.Println("Which graphic would you like to assign this image to?")
+func Select(reader *bufio.Reader) (int, int) {
+	fmt.Println("Which graphic would you like to assign this image to? (1-4)")
 	text, _ := reader.ReadString('\n')
 	text = CleanLineString(text)
 	if text == "1" || text == "2" || text == "3" || text == "4" {
 		fmt.Printf("You have selected graphic: %s\n", text)
 		i, _ := strconv.Atoi(text)
-		return i - 1
+		i -= 1
+		fmt.Println("Please enter the Skate 3 PID: ")
+
+		pid, _ := reader.ReadString('\n')
+		pid = CleanLineString(pid)
+		id, _ := strconv.Atoi(pid)
+		return i, id
 	} else {
 		log.Panic("[ERROR] Incorrect graphic option")
 	}
-	return 0
+	return 0, 0
 }
 
 func main() {
@@ -56,8 +62,22 @@ Please drag & drop your PSG/DDS Graphic into here`)
 	}
 	switch {
 	case strings.HasSuffix(clean, ".psg"):
-		choice := Select(reader)
-		importer.HandlePSG(choice, clean)
+		n, pid := Select(reader)
+		h := importer.Attach(pid)
+		switch n {
+		case 1:
+			importer.ModifyGraphic(h, clean, 0x70, importer.GraphicOne)
+		case 2:
+			importer.ModifyGraphic(h, clean, 0x70, importer.GraphicTwo)
+		case 3:
+			importer.ModifyGraphic(h, clean, 0x70, importer.GraphicThree)
+		case 4:
+			importer.ModifyGraphic(h, clean, 0x70, importer.GraphicFour)
+		default:
+			log.Panic("[ERROR] Incorrect graphic option")
+		}
+
+		//importer.HandlePSG(choice, clean)
 	case strings.HasSuffix(clean, ".dds"):
 		//choice := Select(reader)
 	default:
--- importer/process.go
+++ importer/process.go
@@ -1,5 +1,76 @@
 package importer
 
-func Attach() {
+import (
+	"bytes"
+	"converter"
+	"io"
+	"log"
+	"os"
+	"unsafe"
+	"golang.org/x/sys/windows"
+)
+const (
+	GraphicOne = 0x3427B6EF1
+	GraphicTwo = 0x3427CD119
+	GraphicThree = 0x3427D822D
+	GraphicFour = 0x4427C21C0
+)
+
+func ModifyGraphic(handle windows.Handle, file string, nsize int, address uintptr) {
+	//We make two memory reads, one to get the header and body size of the graphic, two to read the whole graphic to a buffer
+	data, err := os.ReadFile(file)
+	if err != nil {
+		log.Panic(err)
+	}
 	
+	buffer := ReadProcMemory(handle, nsize, address)
+	r := bytes.NewReader(buffer)
+	var g converter.Graphic
+	g.Offset = int64(address)
+
+	r.Seek(0x43, io.SeekStart)
+	g.HeadSize = converter.ReadBEUint32(r)
+
+	r.Seek(0x6B, io.SeekStart)
+	g.BodySize = converter.ReadBEUint32(r)
+	g.Size = g.HeadSize + g.BodySize
+
+	WriteProcMemory(handle, data[g.HeadSize:g.Size], int(g.BodySize), address + uintptr(g.HeadSize))
+	windows.CloseHandle(handle)
+}
+
+func Attach(pid int) (windows.Handle) {
+	handle, err := windows.OpenProcess(0x1F0FFF, false, uint32(pid))
+	if err != nil {
+		log.Panic(err)
+	}
+	return handle
+}
+
+func ReadProcMemory(handle windows.Handle, nSize int, address uintptr) ([]byte) {
+	read := windows.MustLoadDLL("kernel32.dll").MustFindProc("ReadProcessMemory")
+	var data = make([]byte, nSize)
+	var length uint32
+
+	  ret, _, e := read.Call(uintptr(handle), address,
+    	uintptr(unsafe.Pointer(&data[0])),
+    	uintptr(nSize), uintptr(unsafe.Pointer(&length)))
+		if ret == 0 {
+			log.Panic(e)
+		}
+	return data
+}
+
+
+func WriteProcMemory(handle windows.Handle, data []byte,  nSize int, address uintptr) {
+	write := windows.MustLoadDLL("kernel32.dll").MustFindProc("WriteProcessMemory")
+	var length uint32
+
+	  ret, _, e := write.Call(uintptr(handle), address,
+    	uintptr(unsafe.Pointer(&data[0])),
+    	uintptr(nSize), uintptr(unsafe.Pointer(&length)))
+		if ret == 0 {
+			log.Panic(e)
+		}
+	windows.CloseHandle(handle)
 }
